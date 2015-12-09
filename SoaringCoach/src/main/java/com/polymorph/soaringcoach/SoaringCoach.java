761b829fb20f7a9a0ff5f6634ae6c3f1be5dcba8
Johan Pretorius <jpretorius@polymorph.co.za>
2015-12-09 16:37:35 +0200

    Initial

---
+++ SoaringCoach/src/main/java/com/polymorph/soaringcoach/SoaringCoach.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.polymorph.soaringcoach;
+
+/**
+ * @author johanpretorius
+ *
+ */
+public class SoaringCoach {
+
+	/**
+	 * 
+	 */
+	public SoaringCoach() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	//public static void main(String[] args) {
+	//	System.out.println(helloWorld());
+	//}
+
+	private static String helloWorld() {
+		return "Hello World";
+	}
+}
---
+++ SoaringCoach/src/test/java/com/polymorph/SoaringCoach/TestSoaringCoach.java
@@ -0,0 +1,14 @@
+package com.polymorph.SoaringCoach;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSoaringCoach {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
