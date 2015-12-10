086bd6ac72319a97dfa263f18bbfbb96dc47e788
Russel Mupfumira <russel@polymorph.co.za>
2015-12-10 12:26:46 +0200

    Added file upload controller

---
+++ SoaringCoach/src/main/java/com/polymorph/soaringcoach/hello/FileUploadController.java
@@ -0,0 +1,35 @@
+package com.polymorph.soaringcoach.hello;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Created by russel on 15/12/10.
+ */
+@RestController
+public class FileUploadController {
+
+    @RequestMapping(value="/upload", method= RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(
+            @RequestParam("file") MultipartFile file){
+        String name = "test11";
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
+}
