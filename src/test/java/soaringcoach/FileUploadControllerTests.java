944113e41d0ad395a50044a5cb3cb32bfd701563
Johan Pretorius <pretoriusjf@gmail.com>
2017-03-08 11:38:57 +0200

    Applied license terms to all source files. Also re-organised imports in a few places.

--- src/test/java/soaringcoach/FileUploadControllerTests.java
+++ src/test/java/soaringcoach/FileUploadControllerTests.java
@@ -1,3 +1,33 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *   SoaringCoach is a tool for analysing IGC files produced by modern FAI
+ *   flight recorder devices, and providing the pilot with useful feedback
+ *   on how effectively they are flying.    
+ *   Copyright (C) 2017 Johan Pretorius
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   The author can be contacted via email at pretoriusjf@gmail.com, or 
+ *   by paper mail by addressing as follows: 
+ *      Johan Pretorius 
+ *      PO Box 990 
+ *      Durbanville 
+ *      Cape Town 
+ *      7551
+ *      South Africa
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 package soaringcoach;
 
 import java.io.FileInputStream;
