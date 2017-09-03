5c3f92c090d02d4a1ec9f1f7240c7f5bfde5082e
Johan Pretorius <pretoriusjf@gmail.com>
2017-09-03 22:51:00 +0200

    Remove FixedFormat4J in favor of BeanIO
    Several testcases still had dependencies on FixedFormat4J, though the
    main codebase had long ago been migrated to BeanIO:
     - BeanIO is much faster
     - BeanIO is far less chatty on the terminal (and you can't turn this
       off in FF4J)
     - BeanIO allows much better flexibility
    All the dependencies in the testcases were removed.
    
    As a result it was also possible to remove some (newly) unused code from
    the main codebase, like the method that invoked FF4J in the first place.
    
    One of the consequences of doing this, is that some of the test resource
    IGC files could no longer be parsed correctly, since the BeanIO format
    definition requires the file to have no more or less than 1 pilot name
    entry (the IGC file format requires it, and perhaps more importantly -
    the Soaring Coach UI will require it).  Thus
    "HFPLTPILOT:SoaringCoach TestPilot" was added to a number of test
    resource files.
    
    After all these changes, one of the testcases exposed a weakness in the
    BeanIO configuration, which was that the last field of a B record
    (Other) is not mandatory, so this was fixed by setting minimum
    occurrences to zero in igc_mapping.xml.

--- src/test/java/soaringcoach/TestGNSSPoint.java
+++ src/test/java/soaringcoach/TestGNSSPoint.java
@@ -31,65 +31,15 @@
 package soaringcoach;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import soaringcoach.analysis.GNSSPoint;
 import soaringcoach.analysis.GNSSPointFacade;
 
 public class TestGNSSPoint {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void testGNSSPointNE() throws ParseException {
-		GNSSPoint pt = GNSSPoint.createGNSSPoint("testfile", "B0948523340100N01925448EA00261002670080041315512952118-0065-7300100");
-		
-		SimpleDateFormat d = new SimpleDateFormat("HHmmss");
-		
-		assertTrue(
-				"Timestamp not parsed correctly, expected [094852]", 
-				pt.data.timestamp.equals(d.parse("094852")));
-		
-		assertEquals( 
-				33.66833333333333, 
-				pt.x,
-				0.0009);
-		
-		assertEquals(
-				19.42413333333333,
-				pt.y,
-				0.0009);
-	}
-	
-	@Test
-	public void testGNSSPointSW() throws ParseException {
-		GNSSPoint pt = GNSSPoint.createGNSSPoint("testfile", "B1648523340100S01925448WA00261002670080041315512952118-0065-7300100");
-		
-		SimpleDateFormat d = new SimpleDateFormat("HHmmss");
-
-		assertTrue(
-				"Timestamp not parsed correctly, expected [164852]", 
-				pt.data.timestamp.equals(d.parse("164852")));
-		
-		assertEquals( 
-				-33.66833333333333, 
-				pt.x,
-				0.0009);
-		
-		assertEquals(
-				-19.42413333333333,
-				pt.y,
-				0.0009);
-	}
 
 	@Test
 	public void testDecimalizeDegrees() {
@@ -107,18 +57,6 @@ public class TestGNSSPoint {
 		
 		dec_deg = GNSSPointFacade.decimalizeDegrees("00", "0", "W");
 		assertEquals(0.0, dec_deg, 0.0000001);
-	}
-
-	@Test
-	public void testValidBRecord() {
-		boolean is_valid_rec = GNSSPointFacade.isValidBRecord(null);
-		assertEquals("null record should make invalid b record", false, is_valid_rec);
-		
-		is_valid_rec = GNSSPointFacade.isValidBRecord("H...");
-		assertEquals("H record should make invalid b record", false, is_valid_rec);
-		
-		is_valid_rec =  GNSSPointFacade.isValidBRecord("B...");
-		assertEquals("should be valid b record", true, is_valid_rec);
 	}
 
 	@Test
